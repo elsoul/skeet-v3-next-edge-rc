9e32ba9fb54f3ba5a1b8225796913557847de342
KishiTheMechanic <s.kishi@elsoul.nl>
2024-06-05 17:09:11 +0200

    fix not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,22 +3,26 @@ export const runtime = 'edge'
 export default function NotFound() {
   return (
     <>
-      <title>404: This page could not be found.</title>
-      <div style={styles.error}>
-        <div>
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
-            }}
-          />
-          <h1 className="next-error-h1" style={styles.h1}>
-            404
-          </h1>
-          <div style={styles.desc}>
-            <h2 style={styles.h2}>This page could not be found.</h2>
+      <html lang="en">
+        <title>404: This page could not be found.</title>
+        <body>
+          <div style={styles.error}>
+            <div>
+              <style
+                dangerouslySetInnerHTML={{
+                  __html: `body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}`,
+                }}
+              />
+              <h1 className="next-error-h1" style={styles.h1}>
+                404
+              </h1>
+              <div style={styles.desc}>
+                <h2 style={styles.h2}>This page could not be found.</h2>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </body>
+      </html>
     </>
   )
 }
